14bb613baefbd43f834433d7616708c5281d31b7
anant-pawar <anant.c.pawar@gmail.com>
2023-07-24 15:26:04 +0200

    initial commit

---
+++ src/main/java/com/simplesystem/todomanagementservice/TodoManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simplesystem.todomanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplesystem/todomanagementservice/TodoManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplesystem.todomanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
